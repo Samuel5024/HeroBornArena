06fa71976c2370e8a2d8d1f237642490d56b99a9
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-02-03 02:18:32 -0700

    WebBuild

---
+++ Arena/Assets/Scripts/Item_Behavior_Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Behavior_Gun : MonoBehaviour
+{
+    //OnCollsionEntere comes w/ parameter that stores a reference to the Collider that ran into it
+    void OnCollisionEnter(Collision collision)
+    {
+        //Collision class has a property called gameObject
+        //gameObject holds a reference to the colliding GameObject's Collider
+        if (collision.gameObject.name == "Player")
+        {
+            //if the colliding object is the player we'll call the Destroy() method
+            //this.trasnform.gameObject sets the Pivot prefab to be destroyed
+            Destroy(this.transform.gameObject);
+
+            //prints out log to the console that we have collected an item
+            Debug.Log("Here's a Weapon.\nHOORAY! Now try not to die :)");
+        }
+    }
+}
