e307c79e00731b56d428e33682b595771371f37b
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-02-08 17:29:25 -0700

    jumping (sort of)

--- Arena/Assets/Scripts/Player_Behavior.cs
+++ Arena/Assets/Scripts/Player_Behavior.cs
@@ -3,45 +3,55 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-//The purpose of this code is to move the Player capsule while implementing physics through the Rigidbody component
-
 public class Player_Behavior : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
+
     private float vInput;
     private float hInput;
+    private bool jump = false;
+    private Rigidbody _rb; //capsule rigid body info
+    private CapsuleCollider _col;
 
-    //private Rigidbody-type variable contains capsule's Rigidbody component info
-    private Rigidbody _rb;
-
-    //Start fires when scripts in a scene are initialized => when you click PLAY
     void Start()
     {
-        //GetComponent checks if the Rigidbody component type exists
-        _rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>(); //check if RigidBody component exists
+        _col = GetComponent<CapsuleCollider>();
     }
     void Update()
     {
-        //Input.GetAxis("Vertical") detects when up/W or down/S is pressed 
-        vInput = Input.GetAxis("Vertical") * moveSpeed;
-        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
-
-
-        /* We comment out Transform & Rotate so we don't run two different kinds of player control
-         
-        //Translate takes in a Vector3 parameter to move the Player's TRANSFORM component
-        //Time.deltaTime supplies direction & speed Player needs to move
-        //forward/back along the z axis at the speed we calculated
         
-        this.transform.Translate(Vector3.back * vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        vInput = Input.GetAxis("Vertical") * moveSpeed; //detects if up (W) or down (S) is pressed
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;//detects if up left (L) or right(D) is pressed
+
+        if (IsGrounded () && Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+
+
+        /* We comment out Transform & Rotate so we don't run two different kinds of player control 
+            Translate takes in a Vector3 parameter to move the Player's TRANSFORM component
+            Time.deltaTime supplies direction & speed Player needs to move
+            forward/back along the z axis at the speed we calculated
+        
+            this.transform.Translate(Vector3.back * vInput * Time.deltaTime);
+            this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
         */
     }
 
-    //FixedUpdate() is frame independent for physics code
     private void FixedUpdate()
     {
+        if(jump)
+        {
+            //make player jump
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            jump = false;
+        }
         //rotation stores our our left and right rotation
         Vector3 rotation = Vector3.up * hInput;
 
@@ -52,5 +62,17 @@ public class Player_Behavior : MonoBehaviour
         _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
 
         _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, 
+            _col.bounds.min.y, _col.bounds.center.z);
+        
+        bool grounded = Physics.CheckCapsule(_col.bounds.center,
+            capsuleBottom, distanceToGround, groundLayer,
+                QueryTriggerInteraction.Ignore);
+        
+        return grounded;
     }
 }
